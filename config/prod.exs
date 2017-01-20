ae990e68216e8a1c52393048add208a5afb7c79c
Bill Maya <bill.maya@gmail.com>
2017-01-19 22:07:29 -0500

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,22 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+
+# import_config "prod.secret.exs"
+
+config :hello_phoenix, HelloPhoenix.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "aqueous-woodland-63314.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Do not print debug messages in production
+config :logger, level: :info
+
+# Configure your database
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
