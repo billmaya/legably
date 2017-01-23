042a7ea134f9e78847051c017f490e1a9b8d360b
Bill Maya <bill.maya@gmail.com>
2017-01-22 20:03:25 -0500

    Missing comma in config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :legably, Legably.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "aqueous-woodland-63314.herokuapp.com", port: 443],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
