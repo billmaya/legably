10628395fc9ad76ca6d4fb534416fc214a35f3cf
Bill Maya <bill.maya@gmail.com>
2017-02-09 22:37:44 -0500

    Removed struct used for in-memory repository. Added users schema and changeset.

--- web/models/user.ex
+++ web/models/user.ex
@@ -1,8 +1,30 @@
 defmodule Legably.User do
   use Legably.Web, :model
 
-  defstruct [:id, :lawyer, :firm, :first_name, :last_name, :email, :password,
-             :bar_id, :bar_state, :phone, :firm_name, :area_of_focus,
-             :street_address_1, :street_address_2, :city, :state, :zip]
-             
+  # Primary key automatically defined; defaults to :id
+
+  schema "users" do
+    field :lawyer, :boolean
+    field :firm, :boolean
+    field :first_name, :string
+    field :last_name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+    field :bar_id, :string
+    field :bar_state, :string
+    field :phone, :string
+    field :firm_name, :string
+    field :area_of_focus, :string
+    field :street_address_1, :string
+    field :street_address_2, :string
+    field :city, :string
+    field :state, :string
+    field :zip, :string
+  end
+
+  def changeset(model, params \\ :empty) do
+    model
+  end
+
 end
