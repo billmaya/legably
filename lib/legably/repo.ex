e7d51a31031d8fd367faef04fa460efd7ea402c7
Bill Maya <bill.maya@gmail.com>
2017-02-09 22:26:23 -0500

    Commented out namespace to avoid conflict with in-memory repository functions.

--- lib/legably/repo.ex
+++ lib/legably/repo.ex
@@ -1,5 +1,5 @@
 defmodule Legably.Repo do
-  use Ecto.Repo, otp_app: :legably
+#use Ecto.Repo, otp_app: :legably
 
 @moduledoc """
 In memory repository.
@@ -7,15 +7,15 @@ In memory repository.
 
 def all(Legably.User) do
   [%Legably.User{id: "1", lawyer: true, firm: false, first_name: "Bill", last_name: "Maya",
-                     email: "bill.maya@gmail.com", password: "Qwerty1234", bar_id: "1234", bar_state: "RI",
-                phone: "914.434.7258", firm_name: "", area_of_focus: "",
-               street_address_1: "", street_address_2: "", city: "", state: "", zip: ""},
+                 email: "bill.maya@gmail.com", password: "Qwerty1234", bar_id: "1234", bar_state: "RI",
+                 phone: "914.434.7258", firm_name: "", area_of_focus: "",
+                 street_address_1: "", street_address_2: "", city: "", state: "", zip: ""},
    %Legably.User{id: "2", lawyer: false, firm: true, first_name: "Daniel", last_name: "Reilly",
                  email: "daniell.reilly@gmail.com", password: "Qwerty5678", bar_id: "", bar_state: "",
                  phone: "914.434.7258", firm_name: "Reilly & Associates", area_of_focus: "Corporate Law",
                  street_address_1: "60 Dorrance Street", street_address_2: "", city: "Providence", state: "RI", zip: "02903"},
   ]
- end
+end
 
 def all(_module), do: []
 
@@ -25,7 +25,7 @@ end
 
 def get_by(module, params) do
   Enum.find all(module), fn map ->
-  Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
   end
 end
 
