1120476f55f75038d1e3b42d6bf94d8afa104ebf
Bill Maya <bill.maya@gmail.com>
2017-03-12 12:06:40 -0400

    Added authentication and storage of user_id in session to allow access to Index and Show pages.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,5 +1,6 @@
 defmodule Legably.UserController do
   use Legably.Web, :controller
+  plug :authenticate when action in [:index, :show]
 
   def index(conn, _params) do
     users = Repo.all(Legably.User)
@@ -23,10 +24,22 @@ defmodule Legably.UserController do
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Legably.Auth.login(user)
         |> put_flash(:info, "%{user.first_name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page.")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
     end
   end
 
