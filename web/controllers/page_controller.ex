22e2e1d1e49ee70076e1350f6742e95444fc3b44
Bill Maya <bill.maya@gmail.com>
2017-03-14 23:51:30 -0400

    Added create function to controller.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -3,9 +3,22 @@ defmodule Legably.PageController do
 
   alias Legably.User
 
-  def new(conn, _params) do
+  def index(conn, _params) do
     changeset = User.changeset(%User{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.registration_changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> Legably.Auth.login(user)
+        |> put_flash(:info, "#{user.first_name} created!")
+        |> redirect(to: page_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 
 end
